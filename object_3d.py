9e5bbd7f9606197d87a2d355c3e1061ac89e1658
DESKTOP-TK\Tium <tiky@kakao.com>
2022-12-21 14:42:29 +0900

    22.12.21 -world Axe 좌표(절대좌표)를 출력(draw) -world Axe 좌표의 Y축을 기준으로 rotate 하는 3d 오브젝트
    참고 링크 https://youtu.be/M_Hx0g5vFko

--- object_3d.py
+++ object_3d.py
@@ -8,6 +8,10 @@ class Object3D:
                                 (0, 0, 1, 1), (0, 1, 1, 1), (1, 1, 1, 1), (1, 0, 1, 1)])  # v = (x,y,z,w)   w = 1
 
         self.faces = np.array([(0, 1, 2, 3), (4, 5, 6, 7), (0, 4, 5, 1), (2, 3, 7, 6), (1, 2, 6, 5), (0, 3, 7, 4)])
+        self.font = pg.font.SysFont('Arial', 30, bold=True)
+        self.color_faces = [(pg.Color('orange'), face) for face in self.faces]
+        self.movement_flag, self.draw_vertexes = True, True
+        self.label = ''
 
         
     def draw(self):
@@ -15,7 +19,8 @@ class Object3D:
         self.movement()
 
     def movement(self):
-        self.rotate_y(pg.time.get_ticks() % 0.005)
+        if self.movement_flag:
+            self.rotate_y(pg.time.get_ticks() % 0.005)
         
     def screen_projection(self):
         vertexes = self.vertexes @ self.render.camera.camera_matrix()
@@ -26,14 +31,19 @@ class Object3D:
         vertexes = vertexes [:, :2] #[x,y,z,w] -> [x,y]
         
         
-        for face in self.faces :
+        for index, color_face in enumerate(self.color_faces) :
+            color, face = color_face
             polygon = vertexes[face]
             if not np.any((polygon == self.render.H_WIDTH) | (polygon == self.render.H_HEIGHT)):
-                pg.draw.polygon(self.render.screen, pg.Color('Orange'), polygon, 3)
-            
-        for vertex in vertexes:
-            if not np.any((vertex == self.render.H_WIDTH) | (vertex == self.render.H_HEIGHT)):
-                pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 6)
+                pg.draw.polygon(self.render.screen, color, polygon, 3)
+                if self.label:
+                    text = self.font.render(self.label[index], True, pg.Color('white'))
+                    self.render.screen.blit(text, polygon[-1])
+
+        if self.draw_vertexes:
+            for vertex in vertexes:
+                if not np.any((vertex == self.render.H_WIDTH) | (vertex == self.render.H_HEIGHT)):
+                    pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 6)
             
 
     def translate(self, pos):
@@ -52,6 +62,15 @@ class Object3D:
         self.vertexes = self.vertexes @ rotate_z(angle)
 
 
+class Axes(Object3D):
+    def __init__(self, render):
+        super().__init__(render)
+        self.vertexes = np.array([(0,0,0,1), (1,0,0,1), (0,1,0,1),(0,0,1,1)])
+        self.faces = np.array([(0, 1), (0, 2), (0, 3)])
+        self.colors = [pg.Color('red'), pg.Color('green'), pg.Color('blue')]
+        self.color_faces = [(color, face) for color, face in zip(self.colors, self.faces)]
+        self.draw_vertexes = False
+        self.label = 'XYZ'
 
 
 
